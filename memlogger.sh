fea7833a2345651755fbcb38dd9ff4b2995eacb2
shajoshi <smjoshi@gmail.com>
2022-04-29 09:52:31 +0530

    Revert "Update memlogger.sh"
    This reverts commit 78a92f818c66a17f560ce9a6f80df15de60c8e01.

--- memlogger.sh
+++ memlogger.sh
@@ -1,17 +1,8 @@
 #!/bin/bash -e
 
-if [ $# -eq 0 ]
-  then
-    echo SLEEPFOR=60
-fi
-
-if echo "$1" | grep -qE '^[0-9]+$'; then
-    echo SLEEPFOR=$1
-fi
-
 echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
 while true; do
     echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
-    sleep $SLEEPFOR
+    sleep 60
 done
 
