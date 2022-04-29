78a92f818c66a17f560ce9a6f80df15de60c8e01
shajoshi <smjoshi@gmail.com>
2022-04-29 09:29:01 +0530

    Update memlogger.sh
    Added argument check

--- memlogger.sh
+++ memlogger.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -e
 
+if [ $# -eq 0 ]
+  then
+    echo SLEEPFOR=60
+fi
+
+if echo "$1" | grep -qE '^[0-9]+$'; then
+    echo SLEEPFOR=$1
+fi
+
 echo "      date     time $(free -m | grep total | sed -E 's/^    (.*)/\1/g')"
 while true; do
     echo "$(date '+%Y-%m-%d %H:%M:%S') $(free -m | grep Mem: | sed 's/Mem://g')"
-    sleep 60
+    sleep $SLEEPFOR
 done
 
